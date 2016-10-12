08b282779f93e2baa50ca8d5861b17f05e540b1a
Harsha Lokavarapu <lokavarapuh@gmail.com>
2016-10-12 12:26:15 -0700

    Bug patches

--- Licensing_Program/config_handling.py
+++ Licensing_Program/config_handling.py
@@ -97,7 +97,18 @@ CONFIG_SCHEMA = {
     "type": "object",
 }
 
-CONFIG_DEFAULT = '{"IgnoredFiles": [".licensing.json", ".git/**", ".gitignore",], "LicenseParameters": {}, "CommentedFiles": {}}'
+CONFIG_DEFAULT = {
+    "IgnoredFiles": [
+        ".licensing.json",
+        ".git/**",
+        ".gitignore",
+    ],
+    "LicenseParameters": {
+            "ProjectName": "",
+            "fullname": ""
+    },
+    "CommentedFiles": {}
+}
 
 
 # antpatterns: https://ant.apache.org/manual/dirtasks.html#patterns
@@ -195,19 +206,7 @@ def load_configfile(cwd=".", info_level=""):
 
 
 def genearate_default_configfile(cwd="."):
-    """After reading input related to the specifics of type of license, code
-    name, list of authors, year of copyright and etc, this function then
-    generates a template json file.
+    """This function generates a template json file.
     """
     with open(CONFIG_FILENAME, 'w') as new_configfile:
-        name_of_license = input("Enter license name:")
-        name_of_code = input("Enter project name:")
-        list_of_authors = eval(input("List of authors:"))
-
-        auto_config = eval(CONFIG_DEFAULT)
-        auto_config["License"] = name_of_license
-        auto_config["LicenseParameters"].update({"ProjectName": name_of_code})
-        auto_config["LicenseParameters"].update({"fullname": list_of_authors})
-        auto_config["CommentedFiles"] = {"**/*.[f|F]90": {"LineCommentStart": "!  ","InsertAtLine": 0}}
-
-        json.dump(auto_config, new_configfile, indent=4)
+        json.dump(CONFIG_DEFAULT, new_configfile, indent=4)
